03b7d86bbea0c5b322b22c98e1691d2da89f02b8
nersonSwift <42254802+nersonSwift@users.noreply.github.com>
2019-02-01 20:25:26 +0500

    Initial Commit

---
+++ VKAnim/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKAnim
+//
+//  Created by Александр Сенин on 01/02/2019.
+//  Copyright © 2019 Александр Сенин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
